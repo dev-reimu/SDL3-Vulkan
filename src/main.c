011a4f737e3f424819643e562bc9aab516bacc71
dev-reimu <github.543ov@slmails.com>
2025-07-29 23:08:13 +0300

    implement SDL Main Callbacks

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
-#include <SDL3/SDL.h>
-#include <stdio.h>
-#include <stdint.h>
+// https://wiki.libsdl.org/SDL3/README-main-functions
 
-int8_t main() {
-    printf("Hello, World!\n");
-    return 0;
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+
+#include <stdio.h>
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+    // Create window + audio devices
 }
+
+SDL_AppResult SDL_AppIterate(void *appstate) { printf("Run.\n"); }
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
