17a891236e5aa71f02d0f6b44ece25962f9fce07
dev-reimu <github.543ov@slmails.com>
2025-07-30 18:36:07 +0300

    return proper SDL states + switch from X to Wayland + crude display mode query

--- src/main.c
+++ src/main.c
@@ -14,54 +14,57 @@ SDL_Window *window;
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     SDL_Log("SDL_AppInit\n");
 
+    SDL_SetHint("SDL_VIDEO_DRIVER", "wayland");
+
     if (SDL_Init(SDL_INIT_VIDEO) == false) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL video: %s\n.", SDL_GetError());
-        return 1;
+        return SDL_APP_FAILURE;
     }
     SDL_Log("Initialized SDL video.");
 
     SDL_DisplayID display_id = SDL_GetPrimaryDisplay();
     if (display_id == 0) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get primary display: %s\n.", SDL_GetError());
-        return 1;
+        return SDL_APP_FAILURE;
     }
     SDL_Log("Found primary display with ID %d.", display_id);
 
-    const SDL_DisplayMode *display_mode = SDL_GetCurrentDisplayMode(display_id);
-    if (display_mode == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get display properties of primary display: %s\n.", SDL_GetError());
-        return 1;
-    }
-    SDL_Log("Found primary display properties.");
+    int width = SDL_GetFullscreenDisplayModes(display_id, NULL)[0]->w;
+    int height = SDL_GetFullscreenDisplayModes(display_id, NULL)[0]->h;
+    float refreshrate = SDL_GetFullscreenDisplayModes(display_id, NULL)[0]->refresh_rate;
 
-    window = SDL_CreateWindow("SDL3-Vulkan Window", 2880, 1800, SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
+    window = SDL_CreateWindow("SDL3-Vulkan Window", width, height, SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
     if (window == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n.", SDL_GetError());
-        return 1;
+        return SDL_APP_FAILURE;
     }
-    SDL_Log("Created borderless fullscreen window with resolution %dx%d@%f.", display_mode->w, display_mode->h, display_mode->refresh_rate);
+    SDL_Log("Created borderless fullscreen window with resolution %dx%d@%f.", width, height, refreshrate);
 
-    int *new_width, *new_height;
-    if (SDL_SetWindowFullscreen(window, true) == false &&
-        SDL_GetWindowSize(window, new_width, new_height) == false) {
+    return SDL_APP_CONTINUE;
+    
+    if (SDL_SetWindowFullscreen(window, true) == false) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not convert window to exclusive fullscreen: %s\n.", SDL_GetError());
-        return 1;
+        return SDL_APP_FAILURE;
     }
-    SDL_Log("Converted window to exclusive fullscreen mode with new resolution of %dx%d.", new_width, new_height);
+    SDL_Log("Converted window to exclusive fullscreen mode.");
 
-    return 0;
+    return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
     //SDL_Log("SDL_AppIterate\n");
 
-    return 0;
+    // Wayland requires a constant update loop for a window to show, 
+    // so SDL_CreateWindow() does nothing on its own.
+    // https://github.com/libsdl-org/SDL/issues/6074
+
+    return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     //SDL_Log("SDL_AppEvent\n");
 
-    return 0;
+    return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
