38c4992cdb3e8e79878e23d230a620f6550874ab
dev-reimu <github.543ov@slmails.com>
2025-07-30 15:21:46 +0300

    clean init with error checking (SDL_INIT_VIDEO skips entire SDL_AppInit now for some reason)

--- src/main.c
+++ src/main.c
@@ -7,59 +7,60 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL_video.h>
 
-#define SDL_HINT_VIDEO_WAYLAND_SCALE_TO_DISPLAY "SDL_VIDEO_WAYLAND_SCALE_TO_DISPLAY"
-
 #include <stdio.h>
 
 VkInstance *instance;
 SDL_DisplayID display_id;
-//SDL_DisplayMode *all_display_modes[];
 const SDL_DisplayMode *display_mode;
 SDL_Window *window;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
-    printf("[SDL Main Callback] Init\n");
+    printf("SDL_AppInit\n");
 
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) == 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL video: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("Initialized SDL video.");
 
     display_id = SDL_GetPrimaryDisplay();
+    if (display_id == 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get primary display: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("Found primary display with ID %d", display_id);
 
-    // Query resolutions
-    // int *count;
-    // SDL_GetFullscreenDisplayModes(display_id, count);
-    // printf("Found %d available resolutions.\n", *count);
-    // if (*count <= 0) {
-    //     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get current display mode: %s\n", SDL_GetError());
-    //     return 1;
-    // }
-    // for (int i = 0; i < *count; i++)
-    //     printf("Resolution %d: %dx%d@%f\n", i, all_display_modes[i]->w, all_display_modes[i]->h, all_display_modes[i]->refresh_rate);
-
-    // Create window with current resolution
     display_mode = SDL_GetCurrentDisplayMode(display_id);
+    if (display_mode == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get display properties of primary display: %s\n", SDL_GetError());
+        return 1;
+    }
+    printf("Found primary display properties.");
+
     window = SDL_CreateWindow("SDL3-Vulkan Window", display_mode->w, display_mode->h, SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
     if (window == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         return 1;
     }
+    printf("Created exclusive fullscreen window with resolution %dx%d@%f", display_mode->w, display_mode->h, display_mode->refresh_rate);
 
     return 0;
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    //printf("[SDL Main Callback] Iterate\n");
+    //printf("SDL_AppIterate\n");
 
     return 0;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-    //printf("[SDL Main Callback] Event\n");
+    //printf("SDL_AppEvent\n");
 
     return 0;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    printf("[SDL Main Callback] Quit\n");
+    printf("SDL_AppQuit\n");
 
     vkDestroyInstance(*instance, NULL);
     SDL_DestroyWindow(window);
