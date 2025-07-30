af37df0ea48a26b1c44f53af31c0e170f5fb041a
dev-reimu <github.543ov@slmails.com>
2025-07-30 09:11:29 +0300

    create fullscreen window + successful interation

--- src/main.c
+++ src/main.c
@@ -1,16 +1,41 @@
 // https://wiki.libsdl.org/SDL3/README-main-functions
 
-#define SDL_MAIN_USE_CALLBACKS
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 #include <stdio.h>
 
+SDL_Window *window;
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
-    // Create window + audio devices
+    printf("[SDL Main Callback] Init\n");
+
+    window = SDL_CreateWindow("Test", 800, 600, SDL_WINDOW_FULLSCREEN);
+    if (window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    return 0;
 }
 
-SDL_AppResult SDL_AppIterate(void *appstate) { printf("Run.\n"); }
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    printf("[SDL Main Callback] Iterate\n");
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {}
+    return 0;
+}
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    printf("[SDL Main Callback] Event\n");
+
+    return 0;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    printf("[SDL Main Callback] Quit\n");
+
+    SDL_DestroyWindow(window);
+    
+    return;
+}
