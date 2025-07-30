1f587e8f2e743b0824c62ba1d7d7ac7f623c7fc6
dev-reimu <github.543ov@slmails.com>
2025-07-30 13:59:41 +0300

    comment out query for all resolutions, as it causes segmentation fault and isn't needed yet

--- src/main.c
+++ src/main.c
@@ -12,9 +12,9 @@
 #include <stdio.h>
 
 VkInstance *instance;
-SDL_DisplayMode **all_display_modes;
-const SDL_DisplayMode *current_display_mode;
 SDL_DisplayID display_id;
+//SDL_DisplayMode *all_display_modes[];
+const SDL_DisplayMode *display_mode;
 SDL_Window *window;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
@@ -22,27 +22,22 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 
     SDL_Init(SDL_INIT_VIDEO);
 
-    // VkInstanceCreateInfo createInfo = {};
-    // createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-
-    // vkCreateInstance(&createInfo, NULL, instance);
-
     display_id = SDL_GetPrimaryDisplay();
 
     // Query resolutions
-    int *count;
-    all_display_modes = SDL_GetFullscreenDisplayModes(display_id, count);
-    printf("Found %d available resolutions.\n", *count);
-    if (*count <= 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get current display mode: %s\n", SDL_GetError());
-        return 1;
-    }
-    for (int i = 0; i < *count; i++)
-        printf("Resolution %d: %dx%d@%f\n", i, all_display_modes[i]->w, all_display_modes[i]->h, all_display_modes[i]->refresh_rate);
+    // int *count;
+    // SDL_GetFullscreenDisplayModes(display_id, count);
+    // printf("Found %d available resolutions.\n", *count);
+    // if (*count <= 0) {
+    //     SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get current display mode: %s\n", SDL_GetError());
+    //     return 1;
+    // }
+    // for (int i = 0; i < *count; i++)
+    //     printf("Resolution %d: %dx%d@%f\n", i, all_display_modes[i]->w, all_display_modes[i]->h, all_display_modes[i]->refresh_rate);
 
     // Create window with current resolution
-    current_display_mode = SDL_GetCurrentDisplayMode(display_id);
-    window = SDL_CreateWindow("SDL3-Vulkan Window", current_display_mode->w, current_display_mode->h, SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
+    display_mode = SDL_GetCurrentDisplayMode(display_id);
+    window = SDL_CreateWindow("SDL3-Vulkan Window", display_mode->w, display_mode->h, SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
     if (window == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
         return 1;
