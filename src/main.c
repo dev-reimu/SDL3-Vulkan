bb673c7a4e58bb8e886dbd7a3d640286297d6c81
dev-reimu <github.543ov@slmails.com>
2025-07-30 15:50:04 +0300

    reimport SDL but SDL_INIT_VIDEO still skips program

--- src/main.c
+++ src/main.c
@@ -2,46 +2,44 @@
 
 #include <vulkan/vulkan.h>
 
-#define SDL_MAIN_USE_CALLBACKS 1
+#define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
 #include <stdio.h>
 
-VkInstance *instance;
-SDL_DisplayID display_id;
-const SDL_DisplayMode *display_mode;
 SDL_Window *window;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     printf("SDL_AppInit\n");
 
+    // Program skips all code after SDL_INIT_VIDEO. I don't know how to fix this. Damn software.
     if (SDL_Init(SDL_INIT_VIDEO) == false) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL video: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL video: %s\n.", SDL_GetError());
         return 1;
     }
     printf("Initialized SDL video.");
 
-    display_id = SDL_GetPrimaryDisplay();
+    SDL_DisplayID display_id = SDL_GetPrimaryDisplay();
     if (display_id == 0) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get primary display: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get primary display: %s\n.", SDL_GetError());
         return 1;
     }
     printf("Found primary display with ID %d", display_id);
 
-    display_mode = SDL_GetCurrentDisplayMode(display_id);
+    const SDL_DisplayMode *display_mode = SDL_GetCurrentDisplayMode(display_id);
     if (display_mode == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get display properties of primary display: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get display properties of primary display: %s\n.", SDL_GetError());
         return 1;
     }
     printf("Found primary display properties.");
 
     window = SDL_CreateWindow("SDL3-Vulkan Window", display_mode->w, display_mode->h, SDL_WINDOW_VULKAN | SDL_WINDOW_FULLSCREEN);
     if (window == NULL) {
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n.", SDL_GetError());
         return 1;
     }
-    printf("Created exclusive fullscreen window with resolution %dx%d@%f", display_mode->w, display_mode->h, display_mode->refresh_rate);
+    printf("Created exclusive fullscreen window with resolution %dx%d@%f.", display_mode->w, display_mode->h, display_mode->refresh_rate);
 
     return 0;
 }
@@ -60,8 +58,7 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
     printf("SDL_AppQuit\n");
-
-    vkDestroyInstance(*instance, NULL);
+    
     SDL_DestroyWindow(window);
     
     return;
